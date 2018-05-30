a66f04081d82a1301809e8a074d07e90c6af4444
Josselin BUILS <josselin.buils@gmail.com>
2018-05-30 21:11:54 +0200

    Try to handle base url

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,4 @@
-/**
- * This file is where you define your application routes and controllers.
- *
- * Start by including the middleware you want to run for every request;
- * you can attach middleware to the pre('routes') and pre('render') events.
- *
- * For simplicity, the default setup for route controllers is for each to be
- * in its own file, and we import all the files in the /routes/views directory.
- *
- * Each of these files is a route controller, and is responsible for all the
- * processing that needs to happen for the route (e.g. loading data, handling
- * form submissions, rendering the view template, etc).
- *
- * Bind each route pattern your application should respond to in the function
- * that is exported from this module, following the examples below.
- *
- * See the Express application routing documentation for more information:
- * http://expressjs.com/api.html#app.VERB
- */
+const express = require('express');
 const keystone = require('keystone');
 const middleware = require('./middleware');
 const importRoutes = keystone.importer(__dirname);
@@ -27,20 +9,22 @@ keystone.pre('render', middleware.flashMessages);
 
 // Import Route Controllers
 const routes = {
-	views: importRoutes('./views'),
+  views: importRoutes('./views'),
 };
 
 // Setup Route Bindings
 module.exports = app => {
-  app.use('/tournoi-volley', app);
+  const router = express.Router();
 
-	// Views
-	app.get('/', routes.views.index);
-	app.get('/blog/:category?', routes.views.blog);
-	app.get('/blog/post/:post', routes.views.post);
-	app.get('/gallery', routes.views.gallery);
-	app.all('/contact', routes.views.contact);
+  // Views
+  router.get('/', routes.views.index);
+  router.get('/blog/:category?', routes.views.blog);
+  router.get('/blog/post/:post', routes.views.post);
+  router.get('/gallery', routes.views.gallery);
+  router.all('/contact', routes.views.contact);
 
-	// NOTE: To protect a route so that only admins can see it, use the requireUser middleware:
-	// app.get('/protected', middleware.requireUser, routes.views.protected);
+  app.use('/tournoi-volley', router);
+
+  // NOTE: To protect a route so that only admins can see it, use the requireUser middleware:
+  // app.get('/protected', middleware.requireUser, routes.views.protected);
 };
