69d85ad3d7bdb1508b8053509bc823c3fd03a70d
Josselin BUILS <josselin.buils@gmail.com>
2018-07-05 00:00:42 +0200

    Adds ranking in menu

--- routes/middleware.js
+++ routes/middleware.js
@@ -20,6 +20,7 @@ module.exports.initLocals = function (req, res, next) {
   res.locals.navLinks = [
     { label: 'Accueil', key: 'home', href: '/' },
     { label: 'Calendrier', key: 'calendar', href: '/calendar' },
+    { label: 'Classement', key: 'ranking', href: '/ranking' },
   ];
   res.locals.user = req.user;
   next();
